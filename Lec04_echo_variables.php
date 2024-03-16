fc9207c1939d033ad940e5d530053667fec240d7
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-16 12:11:54 +0700

    first commit

---
+++ Lec04_echo_variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$username = "Prame";
+echo "<b>Hello".$username."</b>";
+echo '<h1 style="color: red;">Maybe we are finished</h1>';
+Echo '<h1 style="color: red;">Maybe we are finished</h1>';
+
+$product1 = 50;
+$product2 = 25;
+$sum = $product1 + $product2;
+echo "Answer: ".$sum;
+?>
+<h1>Hello</h1>
+</body>
+</html>
---
+++ Lec05_data_types.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    # String
+    $username = "Pameza";
+    $password = 'pame"1234';
+
+    # Integer
+    $product1 = 50;
+    $product2 = -25;
+
+    # Float
+    $x = 3.1234;
+    $y = 10.2519;
+
+    # Boolean
+    $z = true;
+    $a = false;
+
+    # Null
+    $b = null;
+
+    echo '<h1>'.$username.'</h1>';
+
+    $s = 200;
+    $t = 10;
+    $v = $s / $t;
+    echo $v;
+
+
+    ?>
+</body>
+</html>

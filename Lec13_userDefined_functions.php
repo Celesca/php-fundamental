2a4b72e8fc1f10a03d387941f1173b0157bb8e03
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-16 17:41:33 +0700

    add: user-defined functions and return functions

---
+++ Lec13_userDefined_functions.php
@@ -0,0 +1,13 @@
+<?php
+    function printHead() {
+        echo "<h1 style='text-align: center;'>My Website</h1>";
+    }
+
+    function showImage() {
+        echo "<img src='https://cdn.pixabay.com/photo/2023/09/30/21/16/river-8286407_1280.png'></img>";
+    }
+
+    printHead();
+    showImage();
+
+?>

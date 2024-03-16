ffbfa5076ede33e9f16cafb2a343e7a9d64fc6ba
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-16 23:18:11 +0700

    add: return-functions

--- Lec14_return_functions.php
+++ Lec14_return_functions.php
@@ -1,6 +1,20 @@
 <?php
-    function sayHello($name) {
-        echo "Hello ". $name;
+
+    // passed by reference - use & to reference in parameter
+    // passed by value - just send the value not refers to the original variable
+    function sayHello(&$name, $lastName = "None") {
+        $name = "Mr. ". $name;
+        return "Hello ". $name. " " . $lastName;
     }
 
-    sayHello("John");
+
+    $myName = "John";
+    $text = sayHello($myName, "Doe");
+    echo $text;
+
+    function add( $x, $y = 10) {
+        return $x + $y;
+    }
+
+    echo "<br>";
+    echo add(5);
